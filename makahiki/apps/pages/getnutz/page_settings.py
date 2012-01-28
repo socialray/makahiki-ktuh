ec4e1015acee765d98af1c0ffba85fab842ffb57
Yongwen <yongwen.xu@gmail.com>
2012-01-27 14:12:11 -1000

    change layout

--- makahiki/apps/pages/getnutz/page_settings.py
+++ makahiki/apps/pages/getnutz/page_settings.py
@@ -3,8 +3,8 @@ PAGE_TITLE = "Get Nutz"
 BASE_TEMPLATE = "logged_in_base.html"
 
 LAYOUTS = {
-    'DEFAULT' : (
-                  (("upcoming_events", "40%"),  ("smartgrid_game", "60%"), ("scoreboard", "40%"),),
+    'DEFAULT' : (  ("scoreboard", "100%"),
+                  (("upcoming_events", "40%"),  ("smartgrid_game", "60%"),),
                 ),
     'PHONE_PORTRAIT' : ( ("upcoming_events", "100%"),
                          ("smartgrid_game", "100%"),
