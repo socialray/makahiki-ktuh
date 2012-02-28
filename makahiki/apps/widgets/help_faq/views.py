b623ee37f2e39a9e7386c2b66234943182322fb8
Philip Johnson <johnson@hawaii.edu>
2012-02-28 07:35:38 -1000

    Edit format.

--- makahiki/apps/widgets/help_faq/views.py
+++ makahiki/apps/widgets/help_faq/views.py
@@ -2,7 +2,7 @@
 views for Help FAQ widget
 """
 
-from managers.help_mgr.models import HelpTopic
+from apps.managers.help_mgr.models import HelpTopic
 
 def supply(request, page_name):
     """ supply view_objects for widget rendering."""
