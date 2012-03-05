a5dd35f51943ed25c3b4c30bf5099434660046c6
Yongwen <yongwen.xu@gmail.com>
2012-03-04 23:27:31 -1000

    update heroku deploy script and .slugignore

--- deploy/heroku_deploy.sh
+++ deploy/heroku_deploy.sh
@@ -1,9 +1,3 @@
 heroku run python makahiki/manage.py syncdb --noinput
-
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/base_teams.json
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/test_users.json 
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/base_activities.json 
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/base_quests.json 
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/base_help.json 
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/test_posts.json
-heroku run python makahiki/manage.py loaddata makahiki/fixtures/test_prizes.json 
+heroku run python makahiki/manage.py migrate
+heroku run python makahiki/manage.py loaddata makahiki/fixtures/base_*.json
