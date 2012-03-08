3f61285343c35d1b97c4ff6ce71991bfe7736ec7
Philip Johnson <johnson@hawaii.edu>
2012-03-08 11:48:24 -1000

    Trying to deal with readthedocs import errors... again.

--- doc/conf.py
+++ doc/conf.py
@@ -328,6 +328,6 @@ class Mock(object):
 # MOCK_MODULES = ['staticfiles', 'markdown'] 
 
 # Define to the empty list so we can keep this code around for experimentation.
-MOCK_MODULES = [ ]
+MOCK_MODULES = ['staticfiles', 'markdown' ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
