1a913e2af571945e7bfe2e45fe0bd58ca7e137bd
Cam Moore <cammymoore07@gmail.com>
2012-03-18 07:52:23 -1000

    more RIB work.

--- makahiki/apps/css_rules/responsive.py
+++ makahiki/apps/css_rules/responsive.py
@@ -11,6 +11,9 @@ widget.
 RETURN_CLASSES = True
 
 CSS_IMPORTS = """
+"""
+
+LOGGED_IN_CSS_IMPORTS = """
 <link href="{0}css/bootstrap.css" rel="stylesheet">
 <link href="{0}css/bootstrap-responsive.css" rel="stylesheet">
 <link href="{0}css/bootstrap-override.css" rel="stylesheet">
@@ -19,3 +22,136 @@ CSS_IMPORTS = """
 <link rel="stylesheet/less" type="text/css" href="{0}css/theme.less">
 <script src="{0}js/less-1.3.0.min.js" type="text/javascript"></script>
 """
+
+PAGE_CSS_IMPORT = {
+    "home": '<link rel="stylesheet" href="{0}css/{1}/pages/home.css">',
+    "landing": '<link rel="stylesheet" href="{0}css/{1}/pages/landing.css">',
+    "news": '<link rel="stylesheet" href="{0}css/{1}/pages/news'
+            '.css">\n<script src="{0}js/news.js" '
+            'type="text/javascript"></script>',
+    "learn": '<link rel="stylesheet" href="{0}css/{1}/pages/learn.css">',
+    "energy": '<link rel="stylesheet" href="{0}css/{1}/pages/energy'
+              '.css">\n<script src="{0}js/news.js" '
+              'type="text/javascript"></script>',
+    "help": '<link rel="stylesheet" href="{0}css/{1}/pages/help.css">',
+    "win": '<link rel="stylesheet" href="{0}css/{1}/pages/win.css">',
+    "profile": '<link rel="stylesheet" href="{0}css/{1}/pages/profile.css">',
+    "canopy": '<link rel="stylesheet" href="{0}css/{1}/pages/canopy.css">',
+    "status": '<link rel="stylesheet" href="{0}css/{1}/pages/status.css">',
+     }
+
+CSS_IDS = {
+    "landing-sponsors": "content-box",
+    "landing-sponsors-title": "content-box-title",
+
+    "quest-list": "quest-list",
+
+    "home-energy": "home-item",
+    "home-activities": "home-item",
+    "home-news": "home-item",
+    "home-prizes": "home-item",
+    "home-help": "home-item",
+    "home-profile": "home-item",
+
+    "energy-power": "content-box",
+    "energy-scoreboard-box": "content-box",
+    "energy-status-box": "content-box",
+    "energy-power-title": "content-box-title",
+    "energy-scoreboard-title": "content-box-title",
+    "energy-status-title": "content-box-title",
+
+    "activity-events-box": "content-box",
+    "activity-scoreboard-box": "content-box",
+    "activity-categories-box": "content-box",
+    "activity-task-stats-box": "content-box",
+    "activity-task-details-box": "content-box",
+    "activity-events-title": "content-box-title",
+    "activity-categories-title": "content-box-title",
+    "activity-scoreboard-title": "content-box-title",
+    "activity-task-stats-title": "content-box-title",
+    "activity-task-details-title": "content-box-title",
+
+    "news-wall": "content-box",
+    "news-events": "content-box",
+    "news-commitments": "content-box",
+    "news-most-popular": "content-box",
+    "news-members": "content-box",
+    "news-wall-title": "content-box-title",
+    "news-events-title": "content-box-title",
+    "news-commitments-title": "content-box-title",
+    "news-most-popular-title": "content-box-title",
+    "news-members-title": "content-box-title",
+    "team-members": "content-box",
+    "team-members-title": "content-box-title",
+
+    "help-video": "content-box",
+    "help-rules": "content-box",
+    "help-faq": "content-box",
+    "help-ask": "content-box",
+    "help-topic": "content-box",
+    "help-video-title": "content-box-title",
+    "help-rules-title": "content-box-title",
+    "help-faq-title": "content-box-title",
+    "help-ask-title": "content-box-title",
+    "help-topic-title": "content-box-title",
+    "help-video-content": "content-box-contents",
+    "help-rules-content": "content-box-contents",
+    "help-faq-content": "content-box-contents",
+    "help-topic-content": "content-box-contents",
+
+    "profile-form-box": "content-box",
+    "profile-badges-box": "content-box",
+    "profile-history-box": "content-box",
+    "profile-commitments-box": "content-box",
+    "profile-notifications-box": "content-box",
+    "profile-form-title": "content-box-title",
+    "profile-badges-title": "content-box-title",
+    "profile-history-title": "content-box-title",
+    "profile-notifications-title": "content-box-title",
+    "profile-commitments-title": "content-box-title",
+    "profile-form-fb-header": "profile-section-header",
+    "profile-form-general-header": "profile-section-header",
+    "profile-form-contact-header": "profile-section-header",
+    "profile-form-display-name-label": "profile-form-label",
+    "profile-form-picture-label": "profile-form-label",
+    "profile-form-about-label": "profile-form-label",
+    "profile-form-logged-in-label": "profile-form-label",
+    "profile-form-fb-profile-label": "profile-form-label",
+    "profile-form-contact-email-label": "profile-form-label",
+    "profile-form-contact-text-label": "profile-form-label",
+
+    "badge-catalog-box": "content-box",
+    "badge-catalog-title": "content-box-title",
+
+    "avatar-change": "content-box",
+    "avatar-change-title": "content-box-title",
+
+    "prizes-list": "content-box",
+    "prizes-raffle": "content-box",
+    "prizes-raffle-title": "content-box-title",
+    "prizes-list-title": "content-box-title",
+
+    "canopy-quests": "content-box",
+    "canopy-quests-title": "content-box-title",
+    "canopy-viz": "content-box",
+    "canopy-viz-title": "content-box-title",
+    "canopy-feed": "content-box",
+    "canopy-feed-title": "content-box-title",
+    "canopy-members": "content-box",
+    "canopy-members-title": "content-box-title",
+    "canopy-karma": "content-box",
+    "canopy-karma-title": "content-box-title",
+    }
+
+CSS_CLASSES = {
+    "system-post": "news-system-post",
+    "system-post-date-string": "news-system-posted",
+    "system-post-content": "news-system-text",
+    "user-post-avatar": "news-image",
+    "user-post-content": "news-text",
+    "user-post-date-string": "news-posted",
+    "activity-categories-title": "activity-categories-title",
+    "prize-item": "prize",
+    "prize-number": "number",
+    "prize-dialog": "prize-dialog",
+    }
