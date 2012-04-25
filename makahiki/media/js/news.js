19be7ee2696c87b5e04812f324d5143c8752a60f
George Lee <keoki.lee@gmail.com>
2012-04-24 15:25:26 -1000

    Updated the popular tasks page. Added in some javascript that calculates the height of carousel elements before creating the carousel.

--- makahiki/media/js/news.js
+++ makahiki/media/js/news.js
@@ -31,4 +31,19 @@ $(document).ready(function() {
     }
     return false;
   });
+  
+  // Calculate the height of the items in the carousel.
+  // Fix the height of all items to that height.
+  var maxHeight = 45;
+  var height = 0;
+  $("#news-most-popular-actions .item").each(function(index, item) {
+    height = $(item).height();
+    // console.log("height of item is " + height);
+    if (height > maxHeight){
+      height = maxHeight;
+    }
+  });
+  
+  $("#news-most-popular-actions").height(maxHeight);
+  $(".carousel").carousel();
 });
