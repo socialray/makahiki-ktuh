3fae70e2cb4cd863450e5483460c34714bd83740
Cam Moore <cammymoore07@gmail.com>
2012-05-16 10:30:45 -1000

    added -v and --verbose flags to verify.py.

--- makahiki/scripts/verify.py
+++ makahiki/scripts/verify.py
@@ -10,14 +10,38 @@ If any errors, prints output from unsuccessful programs and exits with non-zero 
 import sys
 import commands
 import os
+import getopt
 
 
-def main():
+def main(argv):
+    verbose = 0
+    try:
+        opts, args = getopt.getopt(argv, "v", ["verbose"])
+    except getopt.GetoptError:
+        print "Usage verify.py [-v | --verbose]"
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-v", "--verbose"):
+            verbose = 1
+        
+    if verbose == 1:
+        print "running pep8"
+        
     pep8_command = os.path.join("scripts", "run_pep8.sh")
+    if verbose == 1:
+        print "running pylint"
+        
     pylint_command = os.path.join("scripts", "run_pylint.sh")
     (pep8_status, pep8_output) = commands.getstatusoutput(pep8_command)
     (pylint_status, pylint_output) = commands.getstatusoutput(pylint_command)
+    if verbose == 1:
+        print "cleaning"
+        
     commands.getstatusoutput("python manage.py clean_pyc")
+    if verbose == 1:
+        print "running tests"
+        
     (tests_status, tests_output) = commands.getstatusoutput("python manage.py test")
     if pep8_status:
         print "PEP ERROR OUTPUT:"
@@ -32,4 +56,4 @@ def main():
         sys.exit(1)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
