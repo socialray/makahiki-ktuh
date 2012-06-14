27ccd0bf9f67fe24262a0dd1216f56a7bb03a169
Cam Moore <cammymoore07@gmail.com>
2012-06-14 13:50:48 -1000

    Updated action feedback to show average rating and color actions with lower than average ratings red.

--- makahiki/apps/widgets/action_feedback/feedback_analysis.py
+++ makahiki/apps/widgets/action_feedback/feedback_analysis.py
@@ -25,7 +25,18 @@ def get_feedback_comments(action):
 def get_likert_scale_totals(action):
     """returns the totals for the Likert scale for the given action."""
     query_set = get_action_feedback(action)
+#    data = []
+#        {label: 'foo', data: [[1,300], [2,300], [3,300], [4,300], [5,300]]},
+#        {label: 'bar', data: [[1,800], [2,600], [3,400], [4,200], [5,0]]},
+#        {label: 'baz', data: [[1,100], [2,200], [3,300], [4,400], [5,500]]},
+#    ];
     scale = []
+#    for i in xrange(1,5):
+#        temp = {}
+#        temp['label'] = i
+#        temp['data'] = [query_set.filter(rating=i).count(), 1]
+#        scale.append(temp)
+
     scale.append([0.7, query_set.filter(rating=1).count()])
     scale.append([1.7, query_set.filter(rating=2).count()])
     scale.append([2.7, query_set.filter(rating=3).count()])
@@ -34,9 +45,44 @@ def get_likert_scale_totals(action):
     return scale
 
 
+def get_feedback_average(action):
+    """returns the average feedback rating for the given action."""
+    query_set = get_action_feedback(action)
+    total = 0
+    for i in xrange(1, 6):
+        total += query_set.filter(rating=i).count() * i
+    return total / query_set.count()
+
+
+def get_total_feedback_average():
+    running_total = 0
+    for action in get_actions_with_feedback():
+        running_total += get_feedback_average(action)
+    return running_total / get_actions_with_feedback().count()
+
+
 def get_action_likert_scales():
     """returns the likert scale feedback as a dictionary with the action name as key."""
     ret = {}
     for action in get_actions_with_feedback():
         ret[action.name] = get_likert_scale_totals(action)
+        #ret[action.name + 'ave'] = get_feedback_average(action)
     return ret
+
+
+def build_analysis(action):
+    """creates the feedback analysis for the given action."""
+    analysis = {}
+    analysis['scale'] = get_likert_scale_totals(action)
+    analysis['average'] = get_feedback_average(action)
+    return analysis
+
+
+def get_analysis():
+    """returns the feedback analysis for each of the actions as a dictionary
+    keyed with the action name."""
+    analysis = {}
+    for action in get_actions_with_feedback():
+        analysis[action.name] = build_analysis(action)
+    analysis['overall_average'] = get_total_feedback_average()
+    return analysis
