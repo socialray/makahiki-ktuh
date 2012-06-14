4818e5e23f2755755e71c74ed2c02a1c70239b07
Cam Moore <cammymoore07@gmail.com>
2012-06-13 14:12:22 -1000

    Added initial view for action feedback to status page.

--- makahiki/apps/widgets/action_feedback/views.py
+++ makahiki/apps/widgets/action_feedback/views.py
@@ -1,1 +1,14 @@
 """Views handler for action_feedback rendering."""
+#from apps.widgets.action_feedback.models import ActionFeedback
+from apps.widgets.action_feedback import feedback_analysis
+#from apps.widgets.smartgrid.models import Action
+#from django.db.models.aggregates import Count
+
+
+def supply(request, page_name):
+    """Supply view_objects for the Action_Feedback template."""
+    _ = page_name
+
+    return {
+        "action_feedback": feedback_analysis.get_action_likert_scales()
+    }
