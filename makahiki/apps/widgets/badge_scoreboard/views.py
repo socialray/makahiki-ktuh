b06d379c667800a6d4ccf6893e7afb2ab337e97a
Yongwen <yongwen.xu@gmail.com>
2012-06-25 09:34:26 -1000

    fixed badge_scoreboard error when player is not in a team, fixed admin icons

--- makahiki/apps/widgets/badge_scoreboard/views.py
+++ makahiki/apps/widgets/badge_scoreboard/views.py
@@ -10,14 +10,19 @@ def supply(request, page_name):
     team = user.get_profile().team
     num_results = 10 if page_name != "status" else None
     all_profiles = Profile.objects.annotate(num_badges=Count('badgeaward')).order_by('-num_badges')
-    team_profiles = team.profile_set.annotate(num_badges=Count('badgeaward'))\
-        .order_by('-num_badges')
     if num_results:
         all_profiles = all_profiles[:num_results]
-        team_profiles = team_profiles[:num_results]
+
+    team_profiles = None
+    if team:
+        team_profiles = team.profile_set.annotate(
+            num_badges=Count('badgeaward')).order_by('-num_badges')
+        if num_results:
+            team_profiles = team_profiles[:num_results]
+
     return {
         "no_carousel": page_name == "status",
         "profiles": all_profiles,
         "team": team_profiles,
-        "team_name": team.name
+        "team_name": team.name if team else None,
     }
