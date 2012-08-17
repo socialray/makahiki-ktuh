bb126e491cf5e3b6c0971c6b4b7b5d3d50a3404b
Yongwen <yongwen.xu@gmail.com>
2012-08-17 10:20:02 -1000

    re-org the participation game

--- makahiki/apps/widgets/participation/views.py
+++ makahiki/apps/widgets/participation/views.py
@@ -1,6 +1,7 @@
 """Prepares the views for participation widget."""
+from apps.managers.cache_mgr import cache_mgr
 from apps.managers.team_mgr.models import Team
-from apps.widgets.participation.models import TeamParticipation
+from apps.widgets.participation.models import TeamParticipation, ParticipationSetting
 
 
 def supply(request, page_name):
@@ -9,16 +10,41 @@ def supply(request, page_name):
     _ = request
     _ = page_name
 
-    team_participation = TeamParticipation.objects.all()
-    if not team_participation:
-        team_participation = Team.objects.all()
-        for t in team_participation:
-            t.team = t
-            t.participation = 0
+    return get_participations()
 
-    return {
-        "team_participation": team_participation,
-    }
+
+def get_participations():
+    """returns the team participation in categories."""
+    return_dict = cache_mgr.get_cache("team_participation")
+    if return_dict is None:
+        p_settings, _ = ParticipationSetting.objects.get_or_create(pk=1)
+        return_dict = {
+            "participation_100": [],
+            "participation_75": [],
+            "participation_50": [],
+            "participation_0": [],
+            "p_settings": p_settings}
+        team_participation = TeamParticipation.objects.all()
+
+        if not team_participation:
+            team_participation = Team.objects.all()
+            for p in team_participation:
+                p.team = p
+                p.participation = 0
+                return_dict["participation_0"].append(p)
+        else:
+            for p in team_participation:
+                if p.participation >= 100:
+                    return_dict["participation_100"].append(p)
+                elif p.participation >= 75:
+                    return_dict["participation_75"].append(p)
+                elif p.participation >= 50:
+                    return_dict["participation_50"].append(p)
+                else:
+                    return_dict["participation_0"].append(p)  # less than 50
+        cache_mgr.set_cache("team_participation", return_dict, 3600)
+    print return_dict
+    return return_dict
 
 
 def remote_supply(request, page_name):
